6c77c6e21921980e696513e29e9310a5d4c7219d
Alfian Rochmatul Irman <43021053+alfiannsx98@users.noreply.github.com>
2020-03-05 16:13:54 +0700

    mantap
    SEMANGAT!!

--- AlfianRochmatulIrman_E41181407/crud/application/controllers/crud.php
+++ AlfianRochmatulIrman_E41181407/crud/application/controllers/crud.php
@@ -1,6 +1,5 @@
 <?php 
 
-
 class Crud extends CI_Controller{
 
 	function __construct(){
@@ -33,4 +32,33 @@ class Crud extends CI_Controller{
 		redirect('crud/index');
 	}
 
+	function hapus($id){
+		$where = array('id' => $id);
+		$this->m_data->hapus_data($where,'user');
+		redirect('crud/index');
+	}
+	function edit($id){
+		$where = array('id' => $id);
+		$data['user'] = $this->m_data->edit_data($where,'user')->result();
+		$this->load->view('v_edit',$data);
+	}
+	function update(){
+		$id = $this->input->post('id');
+		$nama = $this->input->post('nama');
+		$alamat = $this->input->post('alamat');
+		$pekerjaan = $this->input->post('pekerjaan');
+	
+		$data = array(
+			'nama' => $nama,
+			'alamat' => $alamat,
+			'pekerjaan' => $pekerjaan
+		);
+	
+		$where = array(
+			'id' => $id
+		);
+	
+		$this->m_data->update_data($where,$data,'user');
+		redirect('crud/index');
+	}
 }
