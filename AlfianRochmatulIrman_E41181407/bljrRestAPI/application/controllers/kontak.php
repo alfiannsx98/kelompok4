5afe263ed873210fde8b1732e970358be879f257
Alfian Rochmatul Irman <43021053+alfiannsx98@users.noreply.github.com>
2020-03-06 14:33:23 +0700

    Update untuk REST API berhasil

--- AlfianRochmatulIrman_E41181407/bljrRestAPI/application/controllers/kontak.php
+++ AlfianRochmatulIrman_E41181407/bljrRestAPI/application/controllers/kontak.php
@@ -36,6 +36,22 @@ class Kontak extends REST_Controller{
             $this->response(array('status' => 'fail', 502));
         }
     }
+    // Memperbarui data kontak yang telah ada
+    function index_put(){
+        $id = $this->put('id');
+        $data = array(
+                'id'    => $this->put('id'),
+                'nama'  => $this->put('nama'),
+                'nomor' => $this->put('nomor')
+        );
+        $this->db->where('id', $id);
+        $update = $this->db->update('telepon', $data);
 
+        if($update){
+            $this->response($data, 200);
+        }else{
+            $this->response(array('status' => 'fail', 502));
+        }
+    }
     //Masukan function selanjutnya disini
 }
