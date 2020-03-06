7c17d0aa713ac02e95059eda27df6da8b8fa456a
Alfian Rochmatul Irman <43021053+alfiannsx98@users.noreply.github.com>
2020-03-06 13:44:17 +0700

    ok

--- AlfianRochmatulIrman_E41181407/crud/application/views/v_admin.php
+++ AlfianRochmatulIrman_E41181407/crud/application/views/v_admin.php
@@ -6,6 +6,7 @@
 <body>
 	<h1>Login berhasil !</h1>
 	<h2>Hai, <?php echo $this->session->userdata("nama"); ?></h2>
+	<!-- pada session ini, data session akan menerima data berupa nama dari userdata("nama") -->
 	<a href="<?php echo base_url('login/logout'); ?>">Logout</a>
 </body>
 </html>
