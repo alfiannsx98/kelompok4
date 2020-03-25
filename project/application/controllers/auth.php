44e73a6e43cab5340fa6a44e9c303e0b4b45db61
Alfian Rochmatul Irman <43021053+alfiannsx98@users.noreply.github.com>
2020-03-26 03:15:40 +0700

    semangat irmaan

---
+++ project/application/controllers/auth.php
@@ -0,0 +1,34 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Auth extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->library('form_validation');
+    }
+    public function index()
+    {
+        if ($this->session->userdata('email'))
+        {
+            redirect('user');
+        }
+        $this->form_validation->set_rules('email', 'Email', 'trim|required|valid_email');
+        $this->form_validation->set_rules('password', 'Password', 'trim|required');
+
+        if($this->form_validation->run() == false)
+        {
+            $data['title'] = 'Login Page';
+            $this->load->view('templates/auth_header', $data);
+            $this->load->view('auth/login');
+            $this->load->view('templates/auth_footer');
+        }else{
+
+            // Jika validasi berhasil dilakukan
+            $this->_login();
+        }
+    }
+}
+
+?>
