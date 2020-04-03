8e603d9ed5200d097300f54a201fb395dddb1839
ansyahdotcom <55609786+ansyahdotcom@users.noreply.github.com>
2020-04-03 17:59:18 +0700

    pend

---
+++ project/application/views/admin/vi_pendaftaran.php
@@ -0,0 +1,395 @@
+<!doctype html>
+<html lang="en">
+
+
+<!-- Mirrored from demos.creative-tim.com/material-dashboard-pro/examples/tables/extended.html by HTTrack Website Copier/3.x [XR&CO'2014], Mon, 20 Mar 2017 21:33:58 GMT -->
+<head>
+    <?php $this->load->view("templates/header.php"); ?>
+</head>
+
+<body>
+    <div class="wrapper">
+        <div class="sidebar" data-active-color="rose" data-background-color="black" data-image="../../assets/img/sidebar-1.jpg">
+            <!--
+        Tip 1: You can change the color of active element of the sidebar using: data-active-color="purple | blue | green | orange | red | rose"
+        Tip 2: you can also add an image using data-image tag
+        Tip 3: you can change the color of the sidebar with data-background-color="white | black"
+    -->
+            <div class="logo">
+                <a href="http://www.creative-tim.com/" class="simple-text">
+                    Creative Tim
+                </a>
+            </div>
+            <div class="logo logo-mini">
+                <a href="http://www.creative-tim.com/" class="simple-text">
+                    Ct
+                </a>
+            </div>
+            <div class="sidebar-wrapper">
+                <div class="user">
+                    <div class="photo">
+                        <img src="../../assets/img/faces/avatar.jpg" />
+                    </div>
+                    <div class="info">
+                        <a data-toggle="collapse" href="#collapseExample" class="collapsed">
+                            Tania Andrew
+                            <b class="caret"></b>
+                        </a>
+                        <div class="collapse" id="collapseExample">
+                            <ul class="nav">
+                                <li>
+                                    <a href="#">My Profile</a>
+                                </li>
+                                <li>
+                                    <a href="#">Edit Profile</a>
+                                </li>
+                                <li>
+                                    <a href="#">Settings</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <ul class="nav">
+                    <li>
+                        <a href="../dashboard.html">
+                            <i class="material-icons">dashboard</i>
+                            <p>Dashboard</p>
+                        </a>
+                    </li>
+                    <li>
+                        <a data-toggle="collapse" href="#pagesExamples">
+                            <i class="material-icons">image</i>
+                            <p>Pages
+                                <b class="caret"></b>
+                            </p>
+                        </a>
+                        <div class="collapse" id="pagesExamples">
+                            <ul class="nav">
+                                <li>
+                                    <a href="../pages/pricing.html">Pricing</a>
+                                </li>
+                                <li>
+                                    <a href="../pages/timeline.html">Timeline</a>
+                                </li>
+                                <li>
+                                    <a href="../pages/login.html">Login Page</a>
+                                </li>
+                                <li>
+                                    <a href="../pages/register.html">Register Page</a>
+                                </li>
+                                <li>
+                                    <a href="../pages/lock.html">Lock Screen Page</a>
+                                </li>
+                                <li>
+                                    <a href="../pages/user.html">User Profile</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </li>
+                    <li>
+                        <a data-toggle="collapse" href="#componentsExamples">
+                            <i class="material-icons">apps</i>
+                            <p>Components
+                                <b class="caret"></b>
+                            </p>
+                        </a>
+                        <div class="collapse" id="componentsExamples">
+                            <ul class="nav">
+                                <li>
+                                    <a href="../components/buttons.html">Buttons</a>
+                                </li>
+                                <li>
+                                    <a href="../components/grid.html">Grid System</a>
+                                </li>
+                                <li>
+                                    <a href="../components/panels.html">Panels</a>
+                                </li>
+                                <li>
+                                    <a href="../components/sweet-alert.html">Sweet Alert</a>
+                                </li>
+                                <li>
+                                    <a href="../components/notifications.html">Notifications</a>
+                                </li>
+                                <li>
+                                    <a href="../components/icons.html">Icons</a>
+                                </li>
+                                <li>
+                                    <a href="../components/typography.html">Typography</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </li>
+                    <li>
+                        <a data-toggle="collapse" href="#formsExamples">
+                            <i class="material-icons">content_paste</i>
+                            <p>Forms
+                                <b class="caret"></b>
+                            </p>
+                        </a>
+                        <div class="collapse" id="formsExamples">
+                            <ul class="nav">
+                                <li>
+                                    <a href="../forms/regular.html">Regular Forms</a>
+                                </li>
+                                <li>
+                                    <a href="../forms/extended.html">Extended Forms</a>
+                                </li>
+                                <li>
+                                    <a href="../forms/validation.html">Validation Forms</a>
+                                </li>
+                                <li>
+                                    <a href="../forms/wizard.html">Wizard</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </li>
+                    <li class="active">
+                        <a data-toggle="collapse" href="#tablesExamples" aria-expanded="true">
+                            <i class="material-icons">grid_on</i>
+                            <p>Tables
+                                <b class="caret"></b>
+                            </p>
+                        </a>
+                        <div class="collapse in" id="tablesExamples">
+                            <ul class="nav">
+                                <li>
+                                    <a href="regular.html">Regular Tables</a>
+                                </li>
+                                <li class="active">
+                                    <a href="extended.html">Pendaftaran PKL</a>
+                                </li>
+                                <li>
+                                    <a href="datatables.net.html">DataTables.net</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </li>
+                    <li>
+                        <a data-toggle="collapse" href="#mapsExamples">
+                            <i class="material-icons">place</i>
+                            <p>Maps
+                                <b class="caret"></b>
+                            </p>
+                        </a>
+                        <div class="collapse" id="mapsExamples">
+                            <ul class="nav">
+                                <li>
+                                    <a href="../maps/google.html">Google Maps</a>
+                                </li>
+                                <li>
+                                    <a href="../maps/fullscreen.html">Full Screen Map</a>
+                                </li>
+                                <li>
+                                    <a href="../maps/vector.html">Vector Map</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </li>
+                    <li>
+                        <a href="../widgets.html">
+                            <i class="material-icons">widgets</i>
+                            <p>Widgets</p>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="../charts.html">
+                            <i class="material-icons">timeline</i>
+                            <p>Charts</p>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="../calendar.html">
+                            <i class="material-icons">date_range</i>
+                            <p>Calendar</p>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="main-panel">
+            <?php $this->load->view("templates/navbar.php");?>
+            <div class="content">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="card">
+                                <div class="card-header card-header-icon" data-background-color="rose">
+                                    <i class="material-icons">assignment</i>
+                                </div>
+                                <h4 class="card-title">Simple Table</h4>
+                                <div class="card-content">
+                                    <div class="table-responsive">
+                                        <table class="table">
+                                            <thead>
+                                                <tr>
+                                                    <th class="text-center">No.</th>
+                                                    <th> Tempat PKL </th>
+                                                    <th> Dosen </th>
+                                                    <th class="text-right">Actions</th>
+                                                </tr>
+                                            </thead>
+                                            <?php foreach($pendaftaran as $pnd){ ?>
+                                            <tbody>
+                                                <tr>
+                                                    <td class="text-center"><?php echo $pnd->ID_PND; ?></td>
+                                                    <td><?php echo $pnd->NAMA_PR; ?></td>
+                                                    <td><?php echo $pnd->NAMA_DS; ?></td>
+                                                    <td class="td-actions text-right">
+                                                    <?php echo anchor('admin/pendaftaran/vi_detail/'.$pnd->ID_PND,'<button type="button" rel="tooltip" class="btn btn-info btn-simple">
+                                                            <i class="material-icons">person</i>
+                                                        </button>'); ?>
+                                                        
+                                                        <button type="button" rel="tooltip" class="btn btn-success btn-simple">
+                                                            <i class="material-icons">edit</i>
+                                                        </button>
+                                                        <button type="button" rel="tooltip" class="btn btn-danger btn-simple">
+                                                            <i class="material-icons">close</i>
+                                                        </button>
+                                                    </td>
+                                                </tr>
+                                            </tbody>
+                                            <?php } ?>
+                                        </table>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        
+                        
+                    </div>
+                </div>
+            </div>
+            <footer class="footer">
+                <div class="container-fluid">
+                    <nav class="pull-left">
+                        <ul>
+                            <li>
+                                <a href="#">
+                                    Home
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    Company
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    Portfolio
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    Blog
+                                </a>
+                            </li>
+                        </ul>
+                    </nav>
+                    <p class="copyright pull-right">
+                        &copy;
+                        <script>
+                            document.write(new Date().getFullYear())
+                        </script>
+                        <a href="http://www.creative-tim.com/">Creative Tim</a>, made with love for a better web
+                    </p>
+                </div>
+            </footer>
+        </div>
+    </div>
+    <div class="fixed-plugin">
+        <div class="dropdown show-dropdown">
+            <a href="#" data-toggle="dropdown">
+                <i class="fa fa-cog fa-2x"> </i>
+            </a>
+            <ul class="dropdown-menu">
+                <li class="header-title"> Sidebar Filters</li>
+                <li class="adjustments-line">
+                    <a href="javascript:void(0)" class="switch-trigger active-color">
+                        <div class="badge-colors text-center">
+                            <span class="badge filter badge-purple" data-color="purple"></span>
+                            <span class="badge filter badge-blue" data-color="blue"></span>
+                            <span class="badge filter badge-green" data-color="green"></span>
+                            <span class="badge filter badge-orange" data-color="orange"></span>
+                            <span class="badge filter badge-red" data-color="red"></span>
+                            <span class="badge filter badge-rose active" data-color="rose"></span>
+                        </div>
+                        <div class="clearfix"></div>
+                    </a>
+                </li>
+                <li class="header-title">Sidebar Background</li>
+                <li class="adjustments-line">
+                    <a href="javascript:void(0)" class="switch-trigger background-color">
+                        <div class="text-center">
+                            <span class="badge filter badge-white" data-color="white"></span>
+                            <span class="badge filter badge-black active" data-color="black"></span>
+                        </div>
+                        <div class="clearfix"></div>
+                    </a>
+                </li>
+                <li class="adjustments-line">
+                    <a href="javascript:void(0)" class="switch-trigger">
+                        <p>Sidebar Mini</p>
+                        <div class="togglebutton switch-sidebar-mini">
+                            <label>
+                                <input type="checkbox" unchecked="">
+                            </label>
+                        </div>
+                        <div class="clearfix"></div>
+                    </a>
+                </li>
+                <li class="adjustments-line">
+                    <a href="javascript:void(0)" class="switch-trigger">
+                        <p>Sidebar Image</p>
+                        <div class="togglebutton switch-sidebar-image">
+                            <label>
+                                <input type="checkbox" checked="">
+                            </label>
+                        </div>
+                        <div class="clearfix"></div>
+                    </a>
+                </li>
+                <li class="header-title">Images</li>
+                <li class="active">
+                    <a class="img-holder switch-trigger" href="javascript:void(0)">
+                        <img src="../../assets/img/sidebar-1.jpg" alt="" />
+                    </a>
+                </li>
+                <li>
+                    <a class="img-holder switch-trigger" href="javascript:void(0)">
+                        <img src="../../assets/img/sidebar-2.jpg" alt="" />
+                    </a>
+                </li>
+                <li>
+                    <a class="img-holder switch-trigger" href="javascript:void(0)">
+                        <img src="../../assets/img/sidebar-3.jpg" alt="" />
+                    </a>
+                </li>
+                <li>
+                    <a class="img-holder switch-trigger" href="javascript:void(0)">
+                        <img src="../../assets/img/sidebar-4.jpg" alt="" />
+                    </a>
+                </li>
+                <li class="button-container">
+                    <div class="">
+                        <a href="http://www.creative-tim.com/product/material-dashboard-pro" target="_blank" class="btn btn-rose btn-block">Buy Now</a>
+                    </div>
+                    <div class="">
+                        <a href="http://www.creative-tim.com/product/material-dashboard" target="_blank" class="btn btn-info btn-block">Get Free Demo</a>
+                    </div>
+                </li>
+                <li class="header-title">Thank you for 95 shares!</li>
+                <li class="button-container">
+                    <button id="twitter" class="btn btn-social btn-twitter btn-round"><i class="fa fa-twitter"></i> &middot; 45</button>
+                    <button id="facebook" class="btn btn-social btn-facebook btn-round"><i class="fa fa-facebook-square"> &middot;</i>50</button>
+                </li>
+            </ul>
+        </div>
+    </div>
+</body>
+<!--   Core JS Files   -->
+<?php $this->load->view("templates/js.php");?>
+
+
+<!-- Mirrored from demos.creative-tim.com/material-dashboard-pro/examples/tables/extended.html by HTTrack Website Copier/3.x [XR&CO'2014], Mon, 20 Mar 2017 21:34:01 GMT -->
+</html>
