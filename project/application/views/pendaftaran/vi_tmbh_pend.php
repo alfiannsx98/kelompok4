e194f2b765fd05c166eda673a6453de2ce2d9c24
ansyahdotcom <55609786+ansyahdotcom@users.noreply.github.com>
2020-04-08 09:13:45 +0700

    Update vi_tmbh_pend.php

--- project/application/views/pendaftaran/vi_tmbh_pend.php
+++ project/application/views/pendaftaran/vi_tmbh_pend.php
@@ -8,7 +8,8 @@
 							<h4 class="card-title">Form Pendaftaran PKL</h4>
 						</div>
 						<div class="card-content">
-							<input type="hidden" name="ID_PND" id="ID_PND" value="<?php echo "pnd".sprintf("%0s", $ID_PND); ?>">
+							<input type="hidden" name="ID_PND" id="ID_PND"
+								value="<?php echo "pnd".sprintf("%0s", $ID_PND); ?>">
 							<div class="row">
 								<label class="col-sm-2 label-on-left">Tempat PKL</label>
 								<div class="col-md-5">
@@ -35,36 +36,58 @@
 									</div>
 								</div>
 							</div>
+							<div class="row">
+								<label class="col-sm-2 label-on-left">Tambah Anggota</label>
+							</div>
+							<div class="row">
+								<label class="col-sm-2 label-on-left">Masukkan NIM</label>
+								<div class="col-md-5">
+									<div class="form-group label-floating is-empty">
+										<input type="text" class="form-control">
+									</div>
+								</div>
+								<button class="btn btn-fill btn-rose">Tambah</button>
+							</div>
+							
 							<button type="submit" class="btn btn-fill btn-rose">Submit</button>
 						</div>
 					</form>
-					<div class="col-md-12">
-						<div class="card">
-							<div class="card-content">
-								<div class="table-responsive">
-									<table class="table">
-										<thead>
-											<tr>
-												<th class="text-center">No.</th>
-												<th> Tempat PKL </th>
-												<th> Dosen </th>
-												<th class="text-right">Actions</th>
-											</tr>
-										</thead>
-										<tbody>
-											<tr>
-												<th>sv</th>
-												<th>skdjvb</th>
-												<th>skvj</th>
-											</tr>
-										</tbody>
-									</table>
-								</div>
-							</div>
-						</div>
-					</div>
 				</div>
 			</div>
 		</div>
+		<div class="row">
+								<div class="col-md-12">
+									<div class="card">
+										<div class="card-header card-header-icon" data-background-color="rose">
+											<i class="material-icons">assignment</i>
+										</div>
+										<h4 class="card-title">Simple Table</h4>
+										<div class="card-content">
+											<div class="table-responsive">
+												<table class="table">
+													<thead>
+														<tr>
+															<th class="text-center">No.</th>
+															<th> Tempat PKL </th>
+															<th> Dosen </th>
+															<th class="text-right">Actions</th>
+														</tr>
+													</thead>
+													<!-- php $nomor=1;?> -->
+													<!-- php foreach($pendaftaran as $pnd){ ?> -->
+													<tbody>
+														<tr>
+															<!-- <td class="text-center">= $nomor++; ?></td> -->
+															<!-- <td>php echo $pnd->NAMA_PR; ?></td> -->
+															<!-- <td>php echo $pnd->NAMA_DS; ?></td> -->
+														</tr>
+													</tbody>
+													<!-- php } ?> -->
+												</table>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
 	</div>
 </div>
