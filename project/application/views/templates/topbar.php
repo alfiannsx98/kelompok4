39fb63cd54731db6256e3390db2e4fb103e63f70
Muhammad Arif Annaili Fitrawan-E41181232 <arlopaz.uye121299@gmail.com>
2020-04-12 20:14:45 +0700

    tambah lonceng lah wkwkwk

--- project/application/views/templates/topbar.php
+++ project/application/views/templates/topbar.php
@@ -18,6 +18,33 @@
             </div>
             <div class="collapse navbar-collapse">
                 <ul class="nav navbar-nav navbar-right">
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                            <i class="material-icons">notifications</i>
+                            <span class="notification">5</span>
+                            <p class="hidden-lg hidden-md">
+                                Notifications
+                                <b class="caret"></b>
+                            </p>
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li>
+                                <a href="#">Mahasiswa</a>
+                            </li>
+                            <li>
+                                <a href="#">Dosen</a>
+                            </li>
+                            <li>
+                                <a href="#">Admin</a>
+                            </li>
+                            <li>
+                                <a href="#">Notifikasi Lain</a>
+                            </li>
+                            <li>
+                                <a href="#">Lain-lain</a>
+                            </li>
+                        </ul>
+                    </li>
                     <label for="" class="navbar-brand">
                         <small><?= $user['email']; ?></small>
                     </label>
