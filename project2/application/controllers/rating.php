e529349df3753991c3c2aea8903f39245d0a3981
Alfian Rochmatul Irman <43021053+alfiannsx98@users.noreply.github.com>
2020-05-12 08:41:10 +0700

    okay lets see, ill sleep 1st

--- project2/application/controllers/rating.php
+++ project2/application/controllers/rating.php
@@ -6,19 +6,94 @@ class rating extends CI_Controller
     public function __construct()
     {
             parent::__construct();
-            $this->load->model('star_rating_model');
+            // $this->load->model('star_rating_model');
             is_logged_in();
     }
+    
     public function index()
     {
         $data['title'] = 'Manajemen Rating';
         $data['user'] = $this->db->get_where('user', ['email'=> $this->session->userdata('email')])->row_array();
 
         $data['rating'] = $this->db->get('rating')->result_array();
+
+        $this->form_validation->set_rules('rating', 'Rating', 'required');
+
+        $query = $this->db->query("SELECT * FROM rating");
+
+        $hitung = $query->num_rows();
+
+        $id_rt = "RT" . $hitung;
+
+        if($this->form_validation->run() == false)
+        {
+            $this->load->view('templates/header', $data);
+            $this->load->view('templates/sidebar', $data);
+            $this->load->view('templates/topbar', $data);
+            $this->load->view('rating/index', $data);
+            $this->load->view('templates/footer');
+        }else{
+            $rating = $this->input->post('rating');
+            $data_rating = [
+                'ID_RT' => $id_rt,
+                'RT'    => $rating
+            ];
+            $this->db->insert('rating', $data_rating);
+            redirect('rating');
+        }
     }
 
-    public function add()
+    public function edit_rating()
     {
+        $data['title'] = 'Manajemen Rating';
+        $data['user'] = $this->db->get_where('user', ['email'=> $this->session->userdata('email')])->row_array();
 
+        $data['rating'] = $this->db->get('rating')->result_array();
+
+        $this->form_validation->set_rules('rating', 'Rating', 'required');
+
+        $query = $this->db->select('rating');
+        $hitung = $query->num_rows();
+
+        if($this->form_validation->run() == false)
+        {
+            $this->load->view('templates/header', $data);
+            $this->load->view('templates/sidebar', $data);
+            $this->load->view('templates/topbar', $data);
+            $this->load->view('rating/index', $data);
+            $this->load->view('templates/footer');
+        }else{
+            $rating = ['rating' => $this->input->post('rating')];
+            $id = $this->input->post('id_rating');
+            $this->db->where('ID_RT', $id);
+            $this->db->update('rating', $rating);
+        }
+    }
+
+    public function hapus_rating()
+    {
+        $data['title'] = 'Manajemen Rating';
+        $data['user'] = $this->db->get_where('user', ['email'=> $this->session->userdata('email')])->row_array();
+
+        $data['rating'] = $this->db->get('rating')->result_array();
+
+        $this->form_validation->set_rules('rating', 'Rating', 'required');
+
+        $query = $this->db->select('rating')->num_rows();
+
+        $id_rt = "RT" . $query;
+
+        if($this->form_validation->run() == false)
+        {
+            $this->load->view('templates/header', $data);
+            $this->load->view('templates/sidebar', $data);
+            $this->load->view('templates/topbar', $data);
+            $this->load->view('rating/index', $data);
+            $this->load->view('templates/footer');
+        }else{
+            $id = $this->input->post('id_rating');
+            $this->db->where('ID_RT', $id);
+            $this->db->delete('rating');
+        }
     }
 }
