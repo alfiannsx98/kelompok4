8e09e282f121cb333a268a8d686031a52c59f45f
Bayu_Agil <bayuagil04@gmail.com>
2020-06-06 21:10:44 +0700

    cek

--- project2/application/views/templates/footer.php
+++ project2/application/views/templates/footer.php
@@ -489,6 +489,94 @@
 		});
 	</script>
 
+	<!-- enable multiple delete -->
+	<script>
+		$(document).ready(function(){
+
+			$("button#check").click(function(){
+				$("button#dellall1").prop('hidden', false);
+				$("button#cls").prop('hidden', false);
+				$("button#check").prop('hidden', true);
+				$("th#all1").prop('hidden', false);
+				$("th#all").prop('hidden', false);
+				$("td#list").prop('hidden', false);
+				$("th#acc").prop('hidden', true);
+				$("td#bt").prop('hidden', true);
+				$("button#add").prop('hidden', true);
+			});
+
+		});
+	</script>
+
+	<!-- disable multiple delete -->
+	<script>
+		$(document).ready(function(){
+
+			$("button#cls").click(function(){
+				$("button#dellall1").prop('hidden', true);
+				$("button#dellall").prop('hidden', true);
+				$("button#cls").prop('hidden', true);
+				$("button#checkall").prop('hidden', false);
+				$("button#uncheckall").prop('hidden', true);
+				$("button#check").prop('hidden', false);
+				$("th#all1").prop('hidden', true);
+				$("th#all").prop('hidden', true);
+				$("td#list").prop('hidden', true);
+				$("input#cb").prop('checked', false);
+				$("th#acc").prop('hidden', false);
+				$("td#bt").prop('hidden', false);
+				$("button#add").prop('hidden', false);
+			});
+
+		});
+	</script>
+
+	<!-- enable check all -->
+	<script>
+		$(document).ready(function(){
+
+			$("button#checkall").click(function(){
+				$("button#uncheckall").prop('hidden', false);
+				$("button#checkall").prop('hidden', true);
+				$("input#cb").prop('checked', true);
+			});
+
+		});
+	</script>
+
+	<!-- disable check all -->
+	<script>
+		$(document).ready(function(){
+
+			$("button#uncheckall").click(function(){
+				$("button#checkall").prop('hidden', false);
+				$("button#uncheckall").prop('hidden', true);
+				$("input#cb").prop('checked', false);
+			});
+		});
+	</script>
+
+
+	<!-- change check all -->
+	<script>
+		$(document).ready(function(){
+
+			$("input#cb").change(function(){
+				$("button#uncheckall").prop('hidden', true);
+				$("button#checkall").prop('hidden', false);
+
+				if($("input#cb:checked").length > 0){
+					$("button#dellall1").prop('hidden', true);
+					$("button#dellall").prop('hidden', false);
+				} else {
+					$("button#dellall1").prop('hidden', false);
+					$("button#dellall").prop('hidden', true);
+				}
+			});
+
+		});
+	</script>
+
 	<!-- <script type="text/javascript">
 		const tombol = document.querySelector('#tbh');
 		tombol.addEventListener('click', function(){
